0d4bd5908500e982c6ca71de17d41bc9dcf467b4
Loïc Hoguin <essen@ninenines.eu>
2020-02-05 15:31:02 +0100

    Complete the upgrade test

--- test/upgrade_SUITE.erl
+++ test/upgrade_SUITE.erl
@@ -12,10 +12,6 @@
 %% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 %% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-%% Much of this code is a duplicate of examples_SUITE
-%% in the cowboy repository, with slight modifications.
-%% @todo Refactor release handling code and move to ct_helper.
-
 -module(upgrade_SUITE).
 -compile(export_all).
 -compile(nowarn_export_all).
@@ -52,7 +48,7 @@ do_find_make_cmd() ->
 			Cmd
 	end.
 
-%% Compile, start and stop releases.
+%% Manipulate the release.
 
 do_get_paths(Example0) ->
 	Example = atom_to_list(Example0),
@@ -68,45 +64,17 @@ do_compile_and_start(Example, Config) ->
 	ct:log("~s~n", [os:cmd(Make ++ " -C " ++ Dir ++ " distclean")]),
 	%% TERM=dumb disables relx coloring.
 	ct:log("~s~n", [os:cmd(Make ++ " -C " ++ Dir ++ " TERM=dumb")]),
+	%% @todo Should move the "1" tarball in the correct directory to avoid a warning on downgrade?
 	ct:log("~s~n", [os:cmd(Rel ++ " stop")]),
 	ct:log("~s~n", [os:cmd(Rel ++ " start")]),
 	timer:sleep(2000),
-	ok.
+	ct:log("~s~n", [os:cmd(Rel ++ " eval 'application:info()'")]).
 
 do_stop(Example) ->
 	{Dir, Rel, Log} = do_get_paths(Example),
 	ct:log("~s~n", [os:cmd("sed -i.bak s/\"2\"/\"1\"/ " ++ Dir ++ "/relx.config")]),
 	ct:log("~s~n", [os:cmd(Rel ++ " stop")]),
-	ct:log("~s~n", [element(2, file:read_file(Log))]),
-	ok.
-
-%% Tests.
-
-upgrade_ranch_one_conn(Config) ->
-	Example = tcp_echo,
-	try
-		do_use_ranch_previous(Example),
-		%% @todo Set PROJECT_VERSION to ranch_prev.
-		do_compile_and_start(Example, Config),
-		%% @todo Here we need to establish a connection and check that it works.
-			%% Here we must update the Ranch version to *master* since
-			%% this is what we want to test.
-		%% @todo Set PROJECT_VERSION to ranch_next if using a commit.
-		do_use_ranch_commit(Example, "master"),
-		do_build_relup(Example),
-		do_upgrade(Example),
-		%% @todo Check the Ranch version.
-		%% @todo Check that our connection is still up.
-		%% @todo Check that new connections still work.
-		do_downgrade(Example),
-		%% @todo Check that our connection is still up.
-		%% @todo Check that new connections still work.
-		ok
-	after
-		do_stop(tcp_echo)
-	end.
-
-%% @todo upgrade_ranch_max_conn
+	ct:log("~s~n", [element(2, file:read_file(Log))]).
 
 %% When we are on a tag (git describe --exact-match succeeds),
 %% we use the tag before that as a starting point. Otherwise
@@ -115,13 +83,14 @@ do_use_ranch_previous(Example) ->
 	TagsOutput = os:cmd("git tag | tr - \~ | sort -V | tr \~ -"),
 	ct:log("~s~n", [TagsOutput]),
 	Tags = string:lexemes(TagsOutput, "\n"),
-	DescribeOutput = os:cmd("git describe --exact-match || echo \"NOT_A_TAG\""),
+	DescribeOutput = os:cmd("git describe --exact-match"),
 	ct:log("~s~n", [DescribeOutput]),
-	Prev = case DescribeOutput of
-		"NOT_A_TAG\n" -> hd(lists:reverse(Tags));
-		_ -> hd(tl(lists:reverse(Tags)))
+	{CommitOrTag, Prev} = case DescribeOutput of
+		"fatal: no tag exactly matches " ++ _ -> {commit, hd(lists:reverse(Tags))};
+		_ -> {tag, hd(tl(lists:reverse(Tags)))}
 	end,
-	do_use_ranch_commit(Example, Prev).
+	do_use_ranch_commit(Example, Prev),
+	CommitOrTag.
 
 %% Replace the current Ranch commit with the one given as argument.
 do_use_ranch_commit(Example, Commit) ->
@@ -132,31 +101,85 @@ do_use_ranch_commit(Example, Commit) ->
 	)]).
 
 %% Remove Ranch and rebuild, this time generating a relup.
-do_build_relup(Example) ->
+do_build_relup(Example, CommitOrTag) ->
 	Make = do_find_make_cmd(),
 	{Dir, _, _} = do_get_paths(Example),
 	ct:log("~s~n", [os:cmd("rm -rf " ++ Dir ++ "/deps/ranch")]),
 	ct:log("~s~n", [os:cmd("sed -i.bak s/\"1\"/\"2\"/ " ++ Dir ++ "/relx.config")]),
-	%% We need Ranch to be fetched first in order to copy the current appup.
+	%% We need Ranch to be fetched first in order to copy the current appup
+	%% and optionally update its version when we are not on a tag.
 	ct:log("~s~n", [os:cmd(Make ++ " -C " ++ Dir ++ " deps")]),
 	ct:log("~s~n", [os:cmd("cp " ++ Dir ++ "/../../ebin/ranch.appup "
 		++ Dir ++ "/deps/ranch/ebin/")]),
+	case CommitOrTag of
+		tag -> ok;
+		commit ->
+			ProjectVersion = os:cmd("grep \"PROJECT_VERSION = \" " ++ Dir ++ "/deps/ranch/Makefile"),
+			ct:log(ProjectVersion),
+			["PROJECT_VERSION = " ++ Vsn0|_] = string:lexemes(ProjectVersion, "\n"),
+			[A, B|Tail] = string:lexemes(Vsn0, "."),
+			Vsn = binary_to_list(iolist_to_binary([A, $., B, ".9", lists:join($., Tail)])),
+			ct:log("Changing Ranch version from ~s to ~s~n", [Vsn0, Vsn]),
+			ct:log("~s~n", [os:cmd(
+				"sed -i.bak s/\"PROJECT_VERSION = .*\"/\"PROJECT_VERSION = " ++ Vsn ++ "\"/ "
+					++ Dir ++ "/deps/ranch/Makefile"
+			)]),
+			%% The version in the appup must be the same as PROJECT_VERSION.
+			ct:log("~s~n", [os:cmd(
+				"sed -i.bak s/\"" ++ Vsn0 ++ "\"/\"" ++ Vsn ++ "\"/ "
+					++ Dir ++ "/deps/ranch/ebin/ranch.appup"
+			)])
+	end,
 	ct:log("~s~n", [os:cmd(Make ++ " -C " ++ Dir ++ " relup")]).
 
 %% Copy the tarball in the correct location and upgrade.
 do_upgrade(Example) ->
 	ExampleStr = atom_to_list(Example),
 	{Dir, Rel, _} = do_get_paths(Example),
-%	ct:log("~s~n", [os:cmd("mkdir "
-%		++ Dir ++ "/_rel/" ++ ExampleStr
-%			++ "_example/releases/2/")]),
 	ct:log("~s~n", [os:cmd("cp "
 		++ Dir ++ "/_rel/" ++ ExampleStr
 			++ "_example/" ++ ExampleStr ++ "_example-2.tar.gz "
 		++ Dir ++ "/_rel/" ++ ExampleStr
 			++ "_example/releases/2/" ++ ExampleStr ++ "_example.tar.gz")]),
-	ct:log("~s~n", [os:cmd(Rel ++ " upgrade \"2\"")]).
+	ct:log("~s~n", [os:cmd(Rel ++ " upgrade \"2\"")]),
+	ct:log("~s~n", [os:cmd(Rel ++ " eval 'application:info()'")]).
 
 do_downgrade(Example) ->
 	{_, Rel, _} = do_get_paths(Example),
-	ct:log("~s~n", [os:cmd(Rel ++ " downgrade \"1\"")]).
+	ct:log("~s~n", [os:cmd(Rel ++ " downgrade \"1\"")]),
+	ct:log("~s~n", [os:cmd(Rel ++ " eval 'application:info()'")]).
+
+%% Tests.
+
+upgrade_ranch_one_conn(Config) ->
+	Example = tcp_echo,
+	Port = 5555,
+	try
+		%% Build and start the example release using the previous Ranch version.
+		CommitOrTag = do_use_ranch_previous(Example),
+		do_compile_and_start(Example, Config),
+		%% Establish a connection and check that it works.
+		{ok, S} = gen_tcp:connect("localhost", Port, [{active, false}, binary]),
+		ok = gen_tcp:send(S, "Hello!"),
+		{ok, <<"Hello!">>} = gen_tcp:recv(S, 0, 1000),
+		%% Update Ranch to master then build a release upgrade.
+		do_use_ranch_commit(Example, "master"),
+		do_build_relup(Example, CommitOrTag),
+		%% Perform the upgrade, then check that our connection is still up.
+		do_upgrade(Example),
+		ok = gen_tcp:send(S, "Hello!"),
+		{ok, <<"Hello!">>} = gen_tcp:recv(S, 0, 1000),
+		%% Check that new connections are still accepted.
+		{ok, S} = gen_tcp:connect("localhost", Port, [{active, false}, binary]),
+		%% Perform the downgrade, then check that our connection is still up.
+		do_downgrade(Example),
+		ok = gen_tcp:send(S, "Hello!"),
+		{ok, <<"Hello!">>} = gen_tcp:recv(S, 0, 1000),
+		%% Check that new connections are still accepted.
+		{ok, S} = gen_tcp:connect("localhost", Port, [{active, false}, binary]),
+		ok
+	after
+		do_stop(tcp_echo)
+	end.
+
+%% @todo upgrade_ranch_max_conn
